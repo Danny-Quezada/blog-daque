f72477b004c48494addf4a6638fe81a5e73f6972
Danny-Quezada <danny.quezada32u@std.uni.edu.ni>
2024-02-07 06:28:36 -0600

    Revert "Adding code block in markdownReact"
    This reverts commit 0f65a37fd3391e1ae4572fd2344219661efa3035.

--- src/components/MarkdownReact.jsx
+++ src/components/MarkdownReact.jsx
@@ -2,36 +2,16 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
-import rehypeHighlight from "rehype-highlight";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-
-export function MarkdownReact({ value }) {
+import rehypeHighlight from 'rehype-highlight'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypePrism from 'rehype-prism-plus'
+export function MarkdownReact({value}) {
   return (
     <Markdown
-      components={{
-        code(props) {
-          const { children, className, node, ...rest } = props;
-          const match = /language-(\w+)/.exec(className || "");
-          return match ? (
-            <SyntaxHighlighter
-              {...rest}
-              PreTag="div"
-              children={String(children).replace(/\n$/, "")}
-              language={match[1]}
-              style={dracula}
-            />
-          ) : (
-            <code {...rest} className={className}>
-              {children}
-            </code>
-          );
-        },
-      }}
       remarkPlugins={[remarkGfm, remarkMath]}
-      rehypePlugins={[rehypeKatex]}
+      rehypePlugins={[rehypeKatex, rehypeHighlight,]}
     >
-      {value}
+     {value}
     </Markdown>
   );
 }
